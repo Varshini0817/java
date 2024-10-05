184f66db31e61e587b3418f30c6c6ea3650831d6
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-05 17:58:51 +0530

    Corrected Fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,16 +3,18 @@ import java.util.*;
 public class Fibonacci {
     public static void main(String[] args) {
         int a = 0;
-        int b = 1,count = 0;
+        int b = 1,count = 2;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number : ");
         int num = sc.nextInt();
-        while(count<=num){
+        System.out.print("The series is : "+a+" "+b+" ");
+        while(count<num){
             int temp = b;
             b=a+b;
             a = temp;
             count++;
+            System.out.print(b+" ");
         }
-        System.out.println("The fibonacci series is : "+b);
+        //System.out.println("The fibonacci series is : "+b);
     }
 }
