7cfc22c8a1cb57ee5818e3e659d22424f088fd74
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-06 12:02:05 +0530

    CountOfAParticularDigit

---
+++ CountOfAParticularDigit.java
@@ -0,0 +1,19 @@
+import javax.sound.midi.SysexMessage;
+import java.util.*;
+public class CountOfAParticularDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.print("Enter a digit for count: ");
+        int d = sc.nextInt();
+        int temp = num,cnt=0;
+        while(num>0){
+            int n = num%10;
+            num/=10;
+            if(n==d) cnt++;
+        }
+        System.out.print("The count of "+d+" in "+temp+" is: "+cnt);
+
+    }
+}
