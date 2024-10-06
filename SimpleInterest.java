6bfa7030ad5d7902194456ff908d81dc3133f198
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-06 14:04:12 +0530

    SimpleInterest

---
+++ SimpleInterest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SimpleInterest {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter principal: ");
+        float p = sc.nextFloat();
+        System.out.print("Enter time period: ");
+        float t = sc.nextFloat();
+        System.out.print("Enter rate of interest: ");
+        float r = sc.nextFloat();
+        float SI = (p*t*r)/100;
+        System.out.println("Simple Interest: "+ SI);
+    }
+}
