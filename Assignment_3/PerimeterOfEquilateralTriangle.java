6c6188594a121e6d5d7ea2a45b1020cb5617776c
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 19:01:26 +0530

    PerimeterOfEquilateralTriangle

---
+++ Assignment_3/PerimeterOfEquilateralTriangle.java
@@ -0,0 +1,12 @@
+package Assignment_3;
+import java.util.*;
+
+public class PerimeterOfEquilateralTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the side of the equilateral triangle: ");
+        float side = sc.nextFloat();
+        //Perimeter of equilateral triangle is 3a
+        System.out.print("The perimeter of the equilateral triangle is : "+ 3*side);
+    }
+}
