c16d4ed6849200f74625a7d9bfbbb6e0d60eed29
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 15:56:59 +0530

    Assignment_2 separation

--- Assignment_2/Calculator.java
+++ Assignment_2/Calculator.java
@@ -1,3 +1,5 @@
+package Assignment_2;
+
 import java.util.*;
 public class Calculator {
     public static void  main(String[] args){
--- Assignment_2/Palindrome.java
+++ Assignment_2/Palindrome.java
@@ -1,3 +1,5 @@
+package Assignment_2;
+
 import java.util.*;
 
 public class Palindrome {
