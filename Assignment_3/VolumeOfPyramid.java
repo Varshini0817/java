c1c0f7cd853865b091a99c9689d1eab2ae786fe7
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 21:46:02 +0530

    VolumeOfPyramid

--- Assignment_3/VolumeOfPyramid.java
+++ Assignment_3/VolumeOfPyramid.java
@@ -1,4 +1,16 @@
 package Assignment_3;
+import java.util.*;
 
 public class VolumeOfPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\tPyramid");
+        System.out.print("Enter area(sq.cm): ");
+        double a = sc.nextDouble();
+        System.out.print("Enter height(cm): ");
+        double h = sc.nextDouble();
+        //Volume of Pyramid = (1/3)*area*height
+        double vol = (( (double) 1 /3)*a*h );
+        System.out.print("The volume of the pyramid is "+vol+" cu.cm");
+    }
 }
