264e1269631f4bd19b3987ea3110bd3cca37dffb
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 17:06:51 +0530

    AreaOfIsoscelesTriangle

--- Assignment_3/AreaOfIsosceles.java
+++ Assignment_3/AreaOfIsosceles.java
@@ -1,4 +1,14 @@
 package Assignment_3;
+import java.util.*;
 
 public class AreaOfIsosceles {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(("Enter base: "));
+        float b = sc.nextFloat();
+        System.out.print(("Enter height: "));
+        float h = sc.nextFloat();
+        //Area of an isosceles triangle = 1/2 * b * h
+        System.out.print("Area of the isosceles triangle is : "+ 0.5*b*h);
+    }
 }
