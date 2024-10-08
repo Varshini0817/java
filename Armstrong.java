d67aa014fdd912b8a8ca520266e6911552a92d39
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 13:00:33 +0530

    Armstrong

---
+++ Armstrong.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int  temp = num,a,cube=0;
+        boolean res ;
+        if(num<=9)
+        {
+            res = true;
+        }
+        else {
+            while(num>0){
+                a =num%10;
+                cube += a*a*a;
+                num/=10;
+            }
+            res = cube == temp;
+        }
+        if(res){
+            System.out.println("It is an armstrong number");
+        }
+        else {
+            System.out.println("It is not an armstrong number ");
+        }
+    }
+}
