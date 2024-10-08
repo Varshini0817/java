775a3c369763f3fcd48f4e472e3e27640623dc10
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 20:20:45 +0530

    AreaOfRhombus

--- Assignment_3/AreaOfRhombus.java
+++ Assignment_3/AreaOfRhombus.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class AreaOfRhombus {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("\tRhombus ");
         System.out.print("Enter diagonal 1(cm): ");
         float d1 = sc.nextFloat();
         System.out.print("Enter diagonal 1(cm): ");
