553bdbeb4c288f0b57f73c9cb88370724dee3101
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 17:23:12 +0530

    AreaOfEquilateralTriangle

---
+++ Assignment_3/AreaOfEquilateralTriangle.java
@@ -0,0 +1,13 @@
+package Assignment_3;
+import java.util.*;
+
+public class AreaOfEquilateralTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the side of the triangle: ");
+        float a = sc.nextFloat();
+        double area = (Math.sqrt(3)/4)*Math.pow(a,2);
+        //Area of an equilateral triangle is (sqrt(3)/4)* a*a
+        System.out.print("Area of an equilateral triangle is: "+area);
+    }
+}
