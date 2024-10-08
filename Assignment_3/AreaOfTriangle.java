73b20a2092dd70236199bf7f951e39c33897e970
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 20:20:17 +0530

    AreaOfTriangle

--- Assignment_3/AreaOfTriangle.java
+++ Assignment_3/AreaOfTriangle.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class AreaOfTriangle {
     public static  void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("\tTriangle ");
         System.out.print(("Enter base(cm): "));
         float b = sc.nextFloat();
         System.out.print(("Enter height(cm): "));
