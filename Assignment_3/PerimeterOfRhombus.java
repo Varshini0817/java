07e76f0d60145aa8ed040b304205c57c4836c1c0
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 19:46:43 +0530

    PerimeterOfRhombus

---
+++ Assignment_3/PerimeterOfRhombus.java
@@ -0,0 +1,12 @@
+package Assignment_3;
+import java.util.*;
+
+public class PerimeterOfRhombus {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter side of the rhombus: ");
+        float s = sc.nextFloat();
+        //Perimeter of rhombus is 4*side
+        System.out.print("The perimeter of rhombus is "+ 4*s );
+    }
+}
