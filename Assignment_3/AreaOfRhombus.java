2a4b0c2c061cc43644d04b9d047bfc402468595b
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 17:13:37 +0530

    AreaOfRhombus

--- Assignment_3/AreaOfRhombus.java
+++ Assignment_3/AreaOfRhombus.java
@@ -8,7 +8,7 @@ public class AreaOfRhombus {
         float d1 = sc.nextFloat();
         System.out.print("Enter diagonal 1: ");
         float d2 = sc.nextFloat();
-
+        //Area of the rhombus = 1/2 * diagonal1 * diagonal2
         System.out.println("Area of the rhombus is: "+ 0.5*d1*d2);
     }
 }
