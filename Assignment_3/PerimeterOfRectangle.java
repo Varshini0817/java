1138466eba86e030cd60b9a34474fd44f38f513d
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 19:38:24 +0530

    PerimeterOfRectangle

---
+++ Assignment_3/PerimeterOfRectangle.java
@@ -0,0 +1,13 @@
+package Assignment_3;
+import java.util.*;
+
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length and breadth of the rectangle : ");
+        float l = sc.nextFloat();
+        float b = sc.nextFloat();
+        //Perimeter of the rectangle is 2(l+b)
+        System.out.print("The perimeter of the rectangle is "+ 2*(l+b));
+    }
+}
