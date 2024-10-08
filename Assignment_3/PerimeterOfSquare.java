3d44cd8438aa01b351a3aaaab49cc78f483abe95
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 19:44:58 +0530

    PerimeterOfSquare

---
+++ Assignment_3/PerimeterOfSquare.java
@@ -0,0 +1,12 @@
+package Assignment_3;
+import java.util.*;
+
+public class PerimeterOfSquare {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter side of the square: ");
+        float s = sc.nextFloat();
+        //Perimeter of square is 4*side
+        System.out.print("The perimeter of square is "+ 4*s );
+    }
+}
