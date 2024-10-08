cbaf71202b93690755c93e3a8449c8c50121316d
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-08 20:24:28 +0530

    AreaOfCircle

--- Assignment_3/AreaOfCircle.java
+++ Assignment_3/AreaOfCircle.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class AreaOfCircle {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter radius of the circle(cm): ");
+        System.out.println("\tCircle");
+        System.out.print("Enter radius (cm): ");
         float r = sc.nextFloat();
 
         double area = 3.14 * r *r;
