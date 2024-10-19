495f6e1db8d7a2c3e1ef7e5fb6a3fbbd3fc064a0
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-19 16:17:28 +0530

    Ceiling

---
+++ BinarySearch/Ceiling.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class Ceiling {
+    //ceiling in a bs  refers to the smallest number in the array that is >= target
+    static int ceiling(int[] arr, int target){
+        int start = 0, end = arr.length-1;
+        if(target>arr[end]) return -1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] > target){
+                end = mid - 1;
+            }
+            else if (arr[mid]< target){
+                start = mid + 1;
+            }
+            else{
+                return mid;// returns index
+            }
+           // System.out.println(start+" "+end+" "+mid+" ");
+        }
+        return arr[start];
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={2,3,5,9,14,16,18};
+        int target = 15;
+        System.out.print("Index is: "+ ceiling(arr, target));
+    }
+}
