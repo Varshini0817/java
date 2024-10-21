27fadc84a085b2acd85a19e965ab701aaf9acb93
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-21 22:09:33 +0530

    Rotated sorted array with duplicate values

--- BinarySearch/RotatedArray.java
+++ BinarySearch/RotatedArray.java
@@ -4,10 +4,20 @@ public class RotatedArray {
     public static void main(String[] args) {
         int[] arr = {3,4,5,6,7,0,1,2};
         int t = 1;
-        System.out.print(search(arr,t));
+        int[] arrD = {2,3,4,5,6,7,2,2,2};
+        System.out.println("Unique values array "+ arr[findPivot(arr)] + " Search index: "+ search(arr, t, true));
+        int t1 = 6;
+        System.out.println("Pivot with duplicate elements in an array "+arr[findPivotWithDuplicates(arr)]+" Search index: "+search(arrD,t1,false));
     }
-    static int search(int[] nums, int t){
-        int pivot = findPivot(nums);
+    static int search(int[] nums, int t,boolean unique){
+        int pivot;
+        if(unique) {
+             pivot = findPivot(nums);
+        }
+        else{
+            pivot = findPivotWithDuplicates(nums);
+        }
+
         //if there is no pivot element , the array is sorted
         if(pivot == -1){
             return binarySearch(nums , t, 0,nums.length-1 );
@@ -56,6 +66,34 @@ public class RotatedArray {
                 //in these cases even if we leave mid , we can encounter pivot in first 2 cases
             } else {
                 start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    static int findPivotWithDuplicates(int[] arr){
+        int start = 0, end = arr.length -1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+
+            if(mid < end && arr[mid]>arr[mid+1]) return mid;
+            if(mid > start && arr[mid]<arr[mid-1]) return mid-1;
+            //skipping duplciates
+            if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                //if start is pivot
+                if(start < end && arr[start] > arr[start+1]) {
+                    return start;
+                }
+                start++;
+                //if end-1 is pivot
+                if(end > start && arr[end] <arr[end-1]) {
+                    return end -1;
+                }
+                end--;
+            } else if (arr[start]<arr[mid] || (arr[start] == arr[mid] && arr[mid] >arr[end])) {
+                start = mid +1;
+            }
+            else{
+                end =  mid - 1;
             }
         }
         return -1;
