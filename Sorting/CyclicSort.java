b2d443b287b7c4dcda7249fb1d7102d397c6fab2
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-25 19:21:25 +0530

    Cyclic sort

---
+++ Sorting/CyclicSort.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    //Cyclic sort works on 1-N or 0-N by placing the values correctly at index-1 or index positions
+    // by just swapping them until the condition is satisfied
+    public static void cyclic(int[] arr){
+        for(int i=0; i< arr.length;i++){
+            while(arr[i] != arr[arr[i]-1]){
+                swap(arr, i, arr[i]-1);
+            }
+        }
+    }
+    public static void swap(int[] arr, int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4,7,8,9,10,6};
+        cyclic(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+}
