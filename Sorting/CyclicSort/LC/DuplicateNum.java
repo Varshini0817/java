1de411ecf702550301b348a33ece6c87ee578001
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-25 22:34:57 +0530

    Duplicate num

--- Sorting/CyclicSort/LC/DuplicateNum.java
+++ Sorting/CyclicSort/LC/DuplicateNum.java
@@ -1,13 +1,32 @@
 package Sorting.CyclicSort.LC;
 
 public class DuplicateNum {
-    public int findDuplicate(int[] nums) {
+    //Find duplicate from the array : 3,1,3,4,2
+    public static int findDuplicate(int[] nums) {
         int i =0;
-
+        while(i< nums.length){
+            if(nums[i] != nums[nums[i]-1]){
+                swap(nums, i, nums[i]-1);
+            }
+            else {
+                i++;
+            }
+        }
+        for(i=0; i<nums.length; i++){
+            if(nums[i] != i+1){
+                return nums[i];
+            }
+        }
+        return -1;
     }
     public static void swap(int[] arr, int first, int second){
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,1,3,4,2};
+        System.out.print(findDuplicate(arr));
     }
 }
