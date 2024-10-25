afcbb69530852b4aaa9a2d30faa9a268a8a7abed
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-10-25 19:58:37 +0530

    Missing number from 0 to n

--- Sorting/CyclicSort/LC/MissingNumber.java
+++ Sorting/CyclicSort/LC/MissingNumber.java
@@ -1,16 +1,21 @@
-package Sorting.CyclicSort;
+package Sorting.CyclicSort.LC;
 
 public class MissingNumber {
     public static int missingNumber(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-
-            while (arr[i] != arr[arr[i]] - 1 ) {
-                swap(arr, i, arr[i] - 1);
+        int i=0;
+        while(i<arr.length){
+            int correct = arr[i];
+            if(arr[i] < arr.length && arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }
+            else {
+                i++;
             }
         }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != arr[i - 1]) {
-                return arr[i];
+        //System.out.print(Arrays.toString(arr));
+        for (i=0; i<arr.length; i++){
+            if(arr[i]!=i){
+                return i;
             }
         }
         return arr.length;
