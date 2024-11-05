86e861f9ed7173bab962348e3fa5d9f0c9c2d145
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-05 22:38:14 +0530

    Fibonacci

--- Practise/Recursion/Fibonacci.java
+++ Practise/Recursion/Fibonacci.java
@@ -7,7 +7,6 @@ public class Fibonacci {
 
     //for a little larger numbers say 50, the program doesn't terminate as there are many repeated function calls
     // and the space consumed is more, we can say recursion is inefficient in such cases
-
     // Time complexity of fibonacci is not 2^n but the golden ratio is 2^n
 
     public static int fibonacci(int n){
