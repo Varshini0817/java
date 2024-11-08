c7263a571803752d5d56b2f2fbc6cc1844c5e5d6
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-09 00:36:16 +0530

    Set ith bit

---
+++ Practise/BitManipulation/SetithBit.java
@@ -0,0 +1,19 @@
+package Practise.BitManipulation;
+
+public class SetithBit {
+    // setting ith bit --> 0 to 1 or 1 as 1
+    public static void main(String[] args) {
+        int num = 56;
+        System.out.println(Integer.toBinaryString(num));
+        int i = 2;
+        //System.out.println( 1 | 1);
+        System.out.println(setithBit(num, i));
+        System.out.println(Integer.toBinaryString(setithBit(num, i)));
+    }
+
+    private static int setithBit(int num, int i) {
+        int ans = num | 1  << (i-1);
+        return ans;
+    }
+
+}
