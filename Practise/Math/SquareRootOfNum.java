d994625451c32acddcade25c6f363acaf900d749
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-09 18:05:29 +0530

    Square root of a num

--- Practise/Math/SquareRootOfNum.java
+++ Practise/Math/SquareRootOfNum.java
@@ -2,23 +2,24 @@ package Practise.Math;
 
 public class SquareRootOfNum {
     public static void main(String[] args) {
-        int num = 78 , p=3;
+        int num = 40 , p=3;
         System.out.printf("%.3f",sqrt(num,p));
     }
     public static double sqrt(int num,int p){
         int s = 0, e = num;
-        int m = s+ (e-s)/2;
         double ans = 0.0;
-        if(m*m == num){
-            return m;
+        while(s<=e)
+        {
+            int m = s+ (e-s)/2;
+            if (m * m == num) {
+                return m;
+            } else if (m * m > num) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
         }
-        else if(m*m > num){
-            e = m-1;
-        }
-        else {
-            s = m+1;
-        }
-
+       ans  = e;
         // to get decimal points
         double incr = 0.1;
         for(int i=0; i<p;i++){
