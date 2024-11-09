b85900e284fb93e05e7c593c5e6c555365c2a29d
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-09 15:26:26 +0530

    XOR from A to B

---
+++ Practise/BitManipulation/XORFromAtoB.java
@@ -0,0 +1,15 @@
+package Practise.BitManipulation;
+
+public class XORFromAtoB {
+    public static void main(String[] args) {
+        int a = 3, b=9;
+        int ans = xor(b) ^ xor(a-1);
+        System.out.println(ans);
+    }
+    public static int xor(int num){
+        if( num % 4 == 0) return num;
+        else if(num % 4 ==1) return 1;
+        else if(num % 4 == 2) return num+1;
+        else return 0;
+    }
+}
