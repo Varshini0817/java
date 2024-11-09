8b0b5a60b544cb68e94d049c75af2549ed85a288
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-09 16:12:52 +0530

    XOR from 0 to N

--- Practise/BitManipulation/XORFrom0toN.java
+++ Practise/BitManipulation/XORFrom0toN.java
@@ -2,12 +2,10 @@ package Practise.BitManipulation;
 
 public class XORFrom0toN {
     public static void main(String[] args) {
-        int num = 9;
-        for(int i=0; i<=10; i++) {
-            System.out.println(xor(i)+" "+i);
-        }
+        int a=3 , b=9;
+        int ans = xor(b) ^ xor(a-1);
+        System.out.println(ans);
     }
-
     private static int xor(int num) {
         if( num % 4 == 0) return num;
         else if( num % 4 == 1 ) return 1;
