5bff2470896d955ec08ee82dd4eef94f6952c3ab
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-09 13:28:00 +0530

    Check if the number is a power of 2

--- Practise/BitManipulation/OnceOutOfThriceRepeatition.java
+++ Practise/BitManipulation/OnceOutOfThriceRepeatition.java
@@ -1,4 +1,26 @@
 package Practise.BitManipulation;
 
 public class OnceOutOfThriceRepeatition {
+    public static void main(String[] args) {
+        int arr[] = { 2,2,3,2,7,7,8,7,8,8};
+        System.out.println(findNum(arr)%3);
+    }
+
+    private static int findNum(int[] arr) {
+        int sum = 0;
+        for(int num: arr){
+            int ans = Integer.parseInt(Integer.toBinaryString(num));
+            sum += ans;
+        }
+        int res = 0,i=0 ;
+        while(sum>0){
+            int dig = sum&1;
+            System.out.print(" dig "+dig);
+            res += Math.pow(2,i) *dig%3;
+            System.out.print(" res "+res);
+            i++;
+            sum=sum>>1;
+        }
+        return res;
+    }
 }
