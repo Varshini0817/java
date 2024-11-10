772a03fb5d406f606548d5dfc37f64dc946f856b
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-10 18:05:44 +0530

    Reverse of a number

---
+++ Practise/Recursion/ReverseOfANum.java
@@ -0,0 +1,19 @@
+package Practise.Recursion;
+
+public class ReverseOfANum {
+    static int sum = 0;
+    public static void rev1(int n){
+        if(n==0){
+            return;
+        }
+        int digit = n%10;
+        sum = sum*10+digit;
+        rev1(n/10);
+    }
+    public static void main(String[] args) {
+        rev1(1234);
+        System.out.print(sum);
+    }
+
+
+}
