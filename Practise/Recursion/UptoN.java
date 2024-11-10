b735643c5cf12925fff305e85bd351af9e4b668e
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-10 16:03:28 +0530

    Numbers upto N

---
+++ Practise/Recursion/UptoN.java
@@ -0,0 +1,14 @@
+package Practise.Recursion;
+
+public class UptoN {
+    public static void main(String[] args) {
+        fun(5);
+    }
+    public static void fun(int n){
+        System.out.print(n+" ");
+        if( n== 1){
+            return;
+        }
+        fun(n-1); // no need to return as the function return type is integer
+    }
+}
