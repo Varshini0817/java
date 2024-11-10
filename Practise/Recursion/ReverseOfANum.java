334be6044f883ae991e99e3f4c071c94c65e9f26
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-10 18:11:49 +0530

    Reverse of a number method 2

--- Practise/Recursion/ReverseOfANum.java
+++ Practise/Recursion/ReverseOfANum.java
@@ -1,6 +1,7 @@
 package Practise.Recursion;
 
 public class ReverseOfANum {
+    //method 1
     static int sum = 0;
     public static void rev1(int n){
         if(n==0){
@@ -11,9 +12,23 @@ public class ReverseOfANum {
         rev1(n/10);
     }
     public static void main(String[] args) {
-        rev1(1234);
-        System.out.print(sum);
+        int n = 124648024;
+        rev1(n);
+        System.out.println(sum);
+        System.out.println(rev2(n));
+    }
+    //method 2
+    public static int rev2(int n){
+        int digits = (int)(Math.log10(n))+1;// no. of digits
+        return helper(n, digits);
     }
 
+    private static int helper(int n, int digits) {
+        if(n%10 == n){// single digit - base case
+            return n;
+        }
+        int rem = n%10;
+        return rem*(int)(Math.pow(10, digits-1))+(helper(n/10, digits-1));
+    }
 
 }
