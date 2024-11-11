2cf9eb0b3c2e4d01693f02362738af032a0e05d2
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-11 22:09:05 +0530

    Right triangle

---
+++ Practise/Recursion/Patterns/RightTri.java
@@ -0,0 +1,20 @@
+package Practise.Recursion.Patterns;
+
+public class RightTri {
+    public static void main(String[] args) {
+        pattern(4,0);
+    }
+    public static void pattern( int r, int c){
+        if(r == 0){ //base condition
+            return;
+        }
+        if(c<r){
+            pattern(r,c+1);
+            System.out.print("*");
+        }
+        else {
+            pattern(r-1,0);
+            System.out.println();
+        }
+    }
+}
