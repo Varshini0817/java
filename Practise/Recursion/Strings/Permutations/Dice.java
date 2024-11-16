fbc1eef538cf685082b66a823c0dad72e4c795f3
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-16 11:44:07 +0530

    Dice target

--- Practise/Recursion/Strings/Permutations/Dice.java
+++ Practise/Recursion/Strings/Permutations/Dice.java
@@ -1,8 +1,18 @@
 package Practise.Recursion.Strings.Permutations;
 
+import java.util.ArrayList;
+
 public class Dice {
     public static void main(String[] args) {
-        dice("",4);
+        int target = 4;
+        dice("",target);
+        System.out.println("\n"+diceRet("",target));
+        int face = 7;
+        int target1 = 6;
+        dice("",target1,face);
+        System.out.print("\n"+diceRet("",target1,face));
+
+
     }
     public static void dice(String p, int target){
         if(target == 0){
@@ -12,5 +22,42 @@ public class Dice {
         for(int i=1; i<=6 && i<= target; i++){
             dice(p+i, target-i);
         }
+    }
+
+    public static ArrayList<String> diceRet(String p , int target){
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=1; i<=6 && i<= target; i++){
+            list.addAll(diceRet(p+i,target-i));
+        }
+        return list;
+    }
+
+    //Dice with custom number of faces
+    public static void dice(String p, int target,int face){
+        if(target == 0){
+            System.out.print(p+" ");
+            return;
+        }
+        for(int i=1; i<=face && i<= target; i++){
+            dice(p+i, target-i,face);
+        }
+    }
+
+    public static ArrayList<String> diceRet(String p , int target, int face){
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=1; i<=face && i<= target; i++){
+            list.addAll(diceRet(p+i,target-i,face));
+        }
+        return list;
     }
 }
