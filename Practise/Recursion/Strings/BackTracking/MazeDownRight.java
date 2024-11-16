1e8e3a7de6ea70beffdf79478e3ab9f227980694
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2024-11-16 12:41:14 +0530

    Paths

--- Practise/Recursion/Strings/BackTracking/MazeDownRight.java
+++ Practise/Recursion/Strings/BackTracking/MazeDownRight.java
@@ -1,8 +1,13 @@
 package Practise.Recursion.Strings.BackTracking;
 
+import java.util.ArrayList;
+
 public class MazeDownRight {
     public static void main(String[] args) {
-        System.out.print(maze(3,3));
+        int r = 3, c = 3;
+        System.out.println(maze(r,c));
+        pathRet("",r,c);
+        System.out.println("\n"+pathRetArr("",r,c));
     }
     public static int maze(int r, int c){
         if(r==1||c==1){
@@ -14,5 +19,34 @@ public class MazeDownRight {
         return left + right;
     }
 
+    // printing paths
+    public static void pathRet(String p, int r, int c){
+        if(r==1 && c==1){
+            System.out.print(p+" ");
+            return;
+        }
+        if(r>1){
+            pathRet(p+'D', r-1,c);
+        }
+        if(c>1){
+            pathRet(p+'R',r,c-1);
+        }
+    }
 
+    //Paths in the form a list
+    public static ArrayList<String> pathRetArr(String p, int r, int c){
+        if(r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1){
+            list.addAll(pathRetArr(p+'D', r-1,c));
+        }
+        if(c>1){
+            list.addAll(pathRetArr(p+'R',r,c-1));
+        }
+        return list;
+    }
 }
